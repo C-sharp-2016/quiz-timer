25f3d49811a182fbb454d4fd935aeb498247b49f
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-06-28 00:34:18 +0800

    timer countdown and screenshots works.

--- quiz-timer/quiz-timer/Form1.Designer.cs
+++ quiz-timer/quiz-timer/Form1.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.hrs = new System.Windows.Forms.Label();
+            this.mins = new System.Windows.Forms.Label();
+            this.secs = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time Spent:";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(30, 229);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(83, 44);
+            this.start.TabIndex = 4;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_click);
+            // 
+            // hrs
+            // 
+            this.hrs.AutoSize = true;
+            this.hrs.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hrs.Location = new System.Drawing.Point(144, 30);
+            this.hrs.Name = "hrs";
+            this.hrs.Size = new System.Drawing.Size(30, 24);
+            this.hrs.TabIndex = 5;
+            this.hrs.Text = "00";
+            // 
+            // mins
+            // 
+            this.mins.AutoSize = true;
+            this.mins.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mins.Location = new System.Drawing.Point(202, 30);
+            this.mins.Name = "mins";
+            this.mins.Size = new System.Drawing.Size(30, 24);
+            this.mins.TabIndex = 6;
+            this.mins.Text = "00";
+            // 
+            // secs
+            // 
+            this.secs.AutoSize = true;
+            this.secs.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secs.Location = new System.Drawing.Point(271, 30);
+            this.secs.Name = "secs";
+            this.secs.Size = new System.Drawing.Size(30, 24);
+            this.secs.TabIndex = 7;
+            this.secs.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(180, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(238, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 24);
+            this.label7.TabIndex = 9;
+            this.label7.Text = ":";
+            // 
+            // stop
+            // 
+            this.stop.Enabled = false;
+            this.stop.Location = new System.Drawing.Point(217, 229);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(84, 44);
+            this.stop.TabIndex = 10;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 285);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.secs);
+            this.Controls.Add(this.mins);
+            this.Controls.Add(this.hrs);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label hrs;
+        private System.Windows.Forms.Label mins;
+        private System.Windows.Forms.Label secs;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button stop;
     }
 }
 
--- quiz-timer/quiz-timer/Form1.cs
+++ quiz-timer/quiz-timer/Form1.cs
@@ -6,15 +6,122 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Forms; 
 
 namespace quiz_timer
 {
     public partial class Form1 : Form
     {
+
+        private System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+        private int seconds = 0, minutes = 0, hours = 0;
+        private string seconds_zero = "", minutes_zero = "", hours_zero = "";
+        private Boolean screenShotsCaptured = false;
+        private Boolean timer_status = true;
+        private int counter = 0;
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+        } 
+
+        private void start_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            timer1 = new System.Windows.Forms.Timer();
+
+            timer1.Tick += new EventHandler(timer1_Tick);
+
+            timer1.Interval = 10;
+
+            b.Enabled = false;
+
+            stop.Enabled = true;
+
+            timer1.Start();
         }
+
+        private void stop_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            start.Enabled = true;
+
+            b.Enabled = false;
+
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            seconds++; 
+            
+            if (seconds > 59)
+            {
+                minutes++;   
+                seconds = 1;
+
+                if (minutes % 10 == 0)
+                {
+                    Take_Screen_Shots();
+                } 
+
+            } 
+            if (minutes > 59)
+            { 
+                hours++;
+                minutes = 1; 
+            }
+               
+            if (seconds.ToString().Length == 1)
+            {
+                seconds_zero = "0"; 
+            }
+            else
+            {
+                seconds_zero = "";
+            } 
+
+            if (minutes.ToString().Length == 1)
+            {
+                minutes_zero = "0";
+            }
+            else
+            {
+                minutes_zero = "";
+            }
+             
+            if (hours.ToString().Length == 1)
+            {
+                hours_zero = "0";
+            }
+            else
+            {
+                hours_zero = "";
+            } 
+
+            secs.Text = seconds_zero + seconds.ToString();
+            mins.Text = minutes_zero + minutes.ToString();
+            hrs.Text  = hours_zero + hours.ToString(); 
+        }
+         
+        private void Take_Screen_Shots()
+        {
+
+            counter++;
+             
+            SendKeys.Send("{PRTSC}");
+
+            Image img = Clipboard.GetImage();
+
+            img.Save("D:\\imagetest/"+ counter  + "captured_name.jpg");
+             
+            Console.WriteLine("Take Screen Shots Now.");
+        }
+
     }
 }
