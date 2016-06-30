7c9370fefe7779ef63a618732b8dea5760fe177c
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-06-30 23:28:46 +0800

    let dashboard draggable

--- quiz-timer/quiz-timer/dashboard.cs
+++ quiz-timer/quiz-timer/dashboard.cs
@@ -10,8 +10,23 @@ using System.Windows.Forms;
 
 namespace quiz_timer
 {
+
+
+
+
+
+
     public partial class dashboard : Form
     {
+        
+        //Global variables;
+        private bool _dragging = false;
+        private Point _offset;
+        private Point _start_point = new Point(0, 0);
+
+
+      
+
         public dashboard()
         {
             InitializeComponent();
@@ -20,6 +35,31 @@ namespace quiz_timer
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void dashboard_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void dashboard_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;  // _dragging is your variable flag
+            _start_point = new Point(e.X, e.Y);
+        }
+
+        private void dashboard_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+
+        private void dashboard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this._start_point.X, p.Y - this._start_point.Y);
+            }
         }
     }
 }
